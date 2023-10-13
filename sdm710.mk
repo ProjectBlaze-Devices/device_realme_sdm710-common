48f4fc65f479f08135adc65cbfd9adffd2f36516
Gabriel Biren <gbiren@google.com>
2023-10-13 17:25:04 +0000

    sdm710-common: Use the AIDL WiFi service Bug: 205044134 Test: Presubmit tests Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -506,7 +506,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwpa_client \
     libwifi-hal-qcom \
